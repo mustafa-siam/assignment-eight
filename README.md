b80b787f308f4c4406743dc54c273486f00c9cbd
mustafa  siame <tinychangesremarkable@gmmail.com>
2025-02-03 18:13:14 +0600

    done readme.md file task

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+# Book Review
+
+This is a simple and responsive website where users see review and rate books. 
+
+## Live Site URL
+you can view the live site here:
+
+## feature
+-**responsive design**:this website adjust mobile screen and desktop screen size
+-**Sorting and Filtering**:users can sort books by rating, publication year, or number of pages
+-**Book Details Page**:A dedicated page for each book, displaying its title, description, rating, reviews, and publication details.
+-**Readlist and whislist**-users can add their favourite book in the readlist and whislist
+-**bar chart**-users can see their readlist book items pages in the bar chart
--- src/Book file/Whislistbooks.jsx
+++ src/Book file/Whislistbooks.jsx
@@ -1,19 +1,28 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getstoredbook } from "./Localstorage";
 import Whislistbook from "./Whislistbook";
-
-
+import { shortcontext } from "../header file/Listbooks";
 const Whislistbooks = () => {
     const books=useLoaderData();
+    const {sortby}=useContext(shortcontext);
     const [whislistbook,setwhislistbook]=useState([]);
     useEffect(()=>{
         const storedid=getstoredbook('whislist');
         if(books.length>0){
             const filterdwhislist=books.filter(book=>storedid.includes(book.id))
+            if(sortby==='rating'){
+                filterdwhislist.sort((a,b)=>b.rating-a.rating)
+            }
+          else if(sortby==='pages'){
+            filterdwhislist.sort((a,b)=>b.totalPages-a.totalPages)
+            }
+          else if(sortby==='year'){
+            filterdwhislist.sort((a,b)=>b.yearOfPublishing-a.yearOfPublishing)
+          }
             setwhislistbook(filterdwhislist)
         }
-    },[books])
+    },[books,sortby])
     return (
         <div className="grid gap-6">
             {
